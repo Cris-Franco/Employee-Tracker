0b92cf983afd88073fc9ef9363c1825847aa561d
Cris Franco <cris.franco219@gmail.com>
2021-02-01 17:30:03 -0500

    created db tables for department, role & employee

--- db/schema.sql
+++ db/schema.sql
@@ -5,9 +5,28 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-/*-- Create Department Table --*/
+/*-- Department Table --*/
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+/*-- Role Table --*/
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+);
+
+/*-- Employee Table --*/
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
     PRIMARY KEY(id)
 );
